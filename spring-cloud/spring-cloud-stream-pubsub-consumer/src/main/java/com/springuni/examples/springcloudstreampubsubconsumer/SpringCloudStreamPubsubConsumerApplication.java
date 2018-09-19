df948e1f551db1fa999b88016f62999fb35e1c9b
Laszlo Csontos <github@laszlocsontos.com>
2018-09-19 19:01:43 +0200

    SUBL-1 Add skeleton projects for Spring Cloud Stream GCP Pub/Sub examples

---
+++ spring-cloud/spring-cloud-stream-pubsub-consumer/src/main/java/com/springuni/examples/springcloudstreampubsubconsumer/SpringCloudStreamPubsubConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.springuni.examples.springcloudstreampubsubconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamPubsubConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamPubsubConsumerApplication.class, args);
+	}
+}
---
+++ spring-cloud/spring-cloud-stream-pubsub-producer/src/main/java/com/springuni/examples/springcloudstreampubsubproducer/SpringCloudStreamPubsubProducerApplication.java
@@ -0,0 +1,12 @@
+package com.springuni.examples.springcloudstreampubsubproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamPubsubProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamPubsubProducerApplication.class, args);
+	}
+}
