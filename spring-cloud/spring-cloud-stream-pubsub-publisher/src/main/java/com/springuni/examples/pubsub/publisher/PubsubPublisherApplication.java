450aeff20f476d7554176a09375daf488495ff71
Laszlo Csontos <github@laszlocsontos.com>
2018-09-20 16:15:46 +0200

    SUBL-1 Rename

---
+++ spring-cloud/spring-cloud-stream-pubsub-publisher/src/main/java/com/springuni/examples/pubsub/publisher/PubsubPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.springuni.examples.pubsub.publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubsubPublisherApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PubsubPublisherApplication.class, args);
+  }
+
+}
---
+++ spring-cloud/spring-cloud-stream-pubsub-subscriber/src/main/java/com/springuni/examples/pubsub/subscriber/PubsubSubscriberApplication.java
@@ -0,0 +1,13 @@
+package com.springuni.examples.pubsub.subscriber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubsubSubscriberApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PubsubSubscriberApplication.class, args);
+  }
+
+}
