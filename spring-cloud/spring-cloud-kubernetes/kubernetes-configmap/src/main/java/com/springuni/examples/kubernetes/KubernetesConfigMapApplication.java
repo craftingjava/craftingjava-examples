c641f0efd24b8695aca5e919554e47ceb329cdd7
Laszlo Csontos <github@laszlocsontos.com>
2018-10-08 11:42:14 +0200

    SUBL-6 Fix dependencies

---
+++ spring-cloud/spring-cloud-kubernetes/kubernetes-configmap/src/main/java/com/springuni/examples/kubernetes/KubernetesConfigMapApplication.java
@@ -0,0 +1,13 @@
+package com.springuni.examples.kubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesConfigMapApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KubernetesConfigMapApplication.class, args);
+  }
+
+}
